850fb7955cd8f47e3609a493ebd1b70f6459a74f
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2025-01-04 21:20:25 -0300

    add sketchpad state for better state transition logic

--- test_tk.py
+++ test_tk.py
@@ -7,7 +7,8 @@ class SketchPad(Canvas):
     def __init__(self, parent, **kwargs) -> None:
         super().__init__(parent, **kwargs)
 
-        self._is_dragging: bool = False
+        self._state = 'idle'
+
         self._current_coords: Tuple[int, int] = (0, 0)
         self._current_line: int = 0
         self._lines: List[int] = []
@@ -32,38 +33,55 @@ class SketchPad(Canvas):
 
         parent.bind('z', self._undo)
 
-
     def _init_line_drag(self, event):
+        if self._state != 'idle':
+            return
+        self._state = 'line_init'
         self._current_coords = self._translate_xy(event)
 
     def _line_drag(self, event):
         xy = self._translate_xy(event)
-        if self._is_dragging:
-            self.coords(self._current_line, *self.coords(self._current_line), *xy)
-        else:
-            line_id = self.create_line(*self._current_coords, *xy, **self._line_style)
-            self._current_line = line_id
-            self._is_dragging = True
+        match self._state:
+            case 'line_init':
+                line_id = self.create_line(*self._current_coords, *xy, **self._line_style)
+                self._current_line = line_id
+                self._state = 'line_draw'
+            case 'line_draw':
+                self.coords(self._current_line, *self.coords(self._current_line), *xy)
+            case _:
+                return
         self._current_coords = xy
 
     def _end_line_drag(self, _):
-        self._lines.append(self._current_line)
-        self._is_dragging = False
-
+        match self._state:
+            case 'line_init':
+                pass
+            case 'line_draw':
+                self._lines.append(self._current_line)
+            case _:
+                return
+        self._state = 'idle'
 
     def _init_scroll_drag(self, event):
+        if self._state != 'idle':
+            return
+        self._state = 'drag'
         self._current_coords = event.x, event.y
         # TODO: find way to use grabbing hand cursor
         self.config(cursor="hand1")
 
     def _scroll_drag(self, event):
+        if self._state != 'drag':
+            return
         xy = event.x, event.y
         self.xview('scroll', self._current_coords[0] - xy[0], 'unit')
         self.yview('scroll', self._current_coords[1] - xy[1], 'unit')
         self._current_coords = xy
 
     def _end_scroll_drag(self, _):
-        self._is_dragging = False
+        if self._state != 'drag':
+            return
+        self._state = 'idle'
         self.config(cursor="")
 
 
